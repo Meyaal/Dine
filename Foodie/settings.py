a09b06c872a7aa3e6e3fb9a5f2f55cbbc8dc96d6
Meyaal <alsuhaili_32@hotmail.com>
2023-06-18 07:58:02 +0000

    change webbsite name

--- Foodie/settings.py
+++ Foodie/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", 'menumaster23.herokuapp.com', '8000-meyaal-dine-6n67yiqlfma.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ["localhost", 'MenuMaster23.herokuapp.com', '8000-meyaal-dine-6n67yiqlfma.ws-eu99.gitpod.io']
 
 
 # Application definition
