7edf37c8d7e9c266cac278b5759c8abf4314902d
Meyaal <alsuhaili_32@hotmail.com>
2023-06-30 12:44:46 +0000

    change database

--- Foodie/settings.py
+++ Foodie/settings.py
@@ -97,9 +97,9 @@ WSGI_APPLICATION = 'Foodie.wsgi.application'
 
 #DATABASES = {
   #'default': {
-    # 'ENGINE': 'django.db.backends.sqlite3',
-    # 'NAME': BASE_DIR / 'db.sqlite3',
-   #}
+   # 'ENGINE': 'django.db.backends.sqlite3',
+    #'NAME': BASE_DIR / 'db.sqlite3',
+  # }
 #}
 
 DATABASES = {
--- menu/tests.py
+++ menu/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from datetime import date
 from .models import Booking
+from django.db import DataError
 
 # Create your tests here.
 
@@ -22,3 +23,17 @@ class BookingModelTest(TestCase):
         booking = Booking.objects.get(id=1)
         field_label = booking._meta.get_field('email').verbose_name
         self.assertEquals(field_label, 'email')
+
+    def test_name_length(self):
+        user = User.objects.create_user(username='test user', password='12345')
+        namn = 'h' * 1024
+        email = 'test@test'
+        guests = '2'
+        time = '10:00'
+        date = '1970-01-01'
+        with self.assertRaises(DataError):
+            booking = Booking.objects.create(
+                user=user, namn=namn, email=email, guests=guests, 
+                time=time, date=date)
+            booking.check
+            booking.save()
